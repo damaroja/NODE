2bdec042aa32e4752c13eb30c3c6355e2f337492
damaroja <ildefonsoarrecife3@hotmail.com>
2024-05-06 13:19:34 +0100

    Update Module-fs.js

--- Node.js, Express, MongoDB & More: The Complete Bootcamp 2024/INICIO/Module-fs.js
+++ Node.js, Express, MongoDB & More: The Complete Bootcamp 2024/INICIO/Module-fs.js
@@ -1,8 +1,12 @@
 
-//Haty que tener una archivo de texto en el mismo directorio llamado "text.txt"
+
+//Hay que tener una archivo de texto en el mismo directorio llamado "text.txt"
 
 const fs = require("fs");
 
 const text = fs.readFileSync("./text.txt", "utf8");
+const text2 = "Hora es 78"
 
 console.log(text);
+
+fs.writeFileSync("./text.txt", text2)
