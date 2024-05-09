687a9b08e2c7072996b7375938bc91d2f22a21c4
damaroja <ildefonsoarrecife3@hotmail.com>
2024-05-09 12:06:04 +0100

    Create ImportExport.js

---
+++ Node.js, Express, MongoDB & More: The Complete Bootcamp 2024/INICIO/ImportExport.js
@@ -0,0 +1,17 @@
+
+const f1 = () => null
+const f2 = () => null
+const f3 = () => null
+
+//para crear un modulo, usamos:
+module.exports = {
+  f1, f2, f3
+}
+
+Si es una clase:
+
+class c1 {
+  
+}
+
+module.exports = c1
